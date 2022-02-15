7ab142c78ac5f8e0e9e3cc363d567c58e7ddb627
Amarswain450 <amarswain450@gmail.com>
2022-02-15 17:26:20 +0530

    'add_logout_functionality'

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -10,13 +10,13 @@ const Sidebar = () => {
             </div>
             <ul className="mt-4">
                 <li className="px-4 cursor-pointer transition-all py-3 text-white flex items-center hover:bg-gray-600">
-                    <i class="bi bi-card-list mr-2 inline-block text-lg"></i> <Link to="/dashboard/products" className="text-base capitalize">products</Link>
+                    <i className="bi bi-card-list mr-2 inline-block text-lg"></i> <Link to="/dashboard/products" className="text-base capitalize">products</Link>
                 </li>
                 <li className="px-4 cursor-pointer transition-all py-3 text-white flex items-center hover:bg-gray-600">
-                    <i class="bi bi-bag-check mr-2 inline-block text-lg"></i> <Link to="/dashboard/products" className="text-base capitalize">orders</Link>
+                    <i className="bi bi-bag-check mr-2 inline-block text-lg"></i> <Link to="/dashboard/products" className="text-base capitalize">orders</Link>
                 </li>
                 <li className="px-4 cursor-pointer transition-all py-3 text-white flex items-center hover:bg-gray-600">
-                    <i class="bi bi-people-fill mr-2 inline-block text-lg"></i> <Link to="/dashboard/products" className="text-base capitalize">customers</Link>
+                    <i className="bi bi-people-fill mr-2 inline-block text-lg"></i> <Link to="/dashboard/products" className="text-base capitalize">customers</Link>
                 </li>
             </ul>
         </div>
--- client/src/redux-toolkit/reducers/authReducer.js
+++ client/src/redux-toolkit/reducers/authReducer.js
@@ -1,9 +1,25 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
+import jwtDecode from 'jwt-decode';
 
 const localStorageAdminToken = window.localStorage.getItem('admin-token');
 
+function verifyToken(){
+  if(localStorageAdminToken){
+    const decodeToken = jwtDecode(localStorageAdminToken);
+    const expireIn = new Date(decodeToken.exp * 1000);
+    if(new Date() > expireIn){
+      localStorage.removeItem('admin-token');
+      return null;
+    }else{
+      return localStorageAdminToken;
+    }
+  }else{
+    return null;
+  }
+}
+
 const initialState = {
-    adminToken: localStorageAdminToken ? localStorageAdminToken : null
+    adminToken: verifyToken()
 } 
 
 const authReducer = createSlice({
@@ -12,9 +28,13 @@ const authReducer = createSlice({
   reducers: {
     setAdminToken: (state, action) => {
       state.adminToken = action.payload
+    },
+    logout: (state) => {
+      localStorage.removeItem('admin-token')
+      state.adminToken = null;
     }
   }
 });
 
-export const { setAdminToken } = authReducer.actions;
+export const { setAdminToken, logout } = authReducer.actions;
 export default authReducer.reducer;
