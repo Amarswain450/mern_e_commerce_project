f0e25a3e7f1034a1254ba5b265c7e762553e457b
Amarswain450 <amarswain450@gmail.com>
2022-02-16 15:06:34 +0530

    'work_with_dashboard'

--- client/src/routes/Routing.jsx
+++ client/src/routes/Routing.jsx
@@ -5,9 +5,12 @@ import {
     Route
 } from "react-router-dom";
 import AuthLogin from '../screens/auth/AuthLogin';
+import Categories from '../screens/dashboard/Categories';
+import CreateCategory from '../screens/dashboard/CreateCategory';
 import Products from '../screens/dashboard/Products';
 import Private from './Private';
 import Public from './Public';
+
 
 const Routing = () => {
     return (
@@ -19,6 +22,8 @@ const Routing = () => {
                     </Route>
                     <Route path="dashboard">
                         <Route path="products" element={<Private><Products /></Private>} />
+                        <Route path="categories" element={<Private><Categories /></Private>} />
+                        <Route path="create-category" element={<Private><CreateCategory /></Private>} />
                     </Route>
                 </Routes>
             </BrowserRouter>
