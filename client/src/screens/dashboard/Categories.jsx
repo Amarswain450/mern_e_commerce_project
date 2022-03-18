b7d86469f1b029eb0de1cb0eecdeeda3e1e5dd4f
Amarswain450 <amarswain450@gmail.com>
2022-03-18 21:02:56 +0530

    pagination completed

--- client/src/screens/dashboard/Categories.jsx
+++ client/src/screens/dashboard/Categories.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {Link, useParams} from "react-router-dom"
+import Pagination from "../../components/Pagination";
 import ScreenHeader from "../../components/ScreenHeader";
 import Spinner from "../../components/Spinner";
 import { clearCategory } from "../../redux-toolkit/reducers/categoryReducer";
@@ -9,7 +10,10 @@ import Wrapper from "./Wrapper"
 
 
 const Categories = () => {
-    const {page} = useParams();
+    let {page} = useParams();
+    if(!page){
+       page = 1;
+    }
     const {success} = useSelector((state) => state.categoryReducer);
     const dispatch = useDispatch();
 
@@ -27,27 +31,32 @@ const Categories = () => {
               <Link to="/dashboard/create-category" className="btn-dark">add categories <i className="bi bi-plus"></i></Link>
            </ScreenHeader>
            {success && <div className="alert-success">{success}</div>}
-           {
-            !isLoading ? data?.categories?.length > 0 && <div>
-              <table className="w-full bg-gray-900 rounded-md">
-                 <thead>
-                    <tr className="border-b border-gray-800 text-left">
-                       <th className="p-3 uppercase text-sm font-medium text-gray-500">name</th>
-                       <th className="p-3 uppercase text-sm font-medium text-gray-500">edit</th>
-                       <th className="p-3 uppercase text-sm font-medium text-gray-500">delete</th>
-                    </tr>
-                 </thead>
-                 <tbody>
-                    {data?.categories?.map(category => (
-                       <tr key={category._id} className="odd:bg-gray-800">
-                          <td className="p-3 capitalize text-sm font-normal text-gray-400">{category.name}</td>
-                          <td className="p-3 capitalize text-sm font-normal text-gray-400"><button>edit</button></td>
-                          <td className="p-3 capitalize text-sm font-normal text-gray-400"><button>delete</button></td>
-                       </tr>
-                    ))}
-                 </tbody>
-              </table>
-           </div> : <Spinner />}
+            {
+               !isLoading ? data?.categories?.length > 0 && 
+               <>
+                  <div>
+                     <table className="w-full bg-gray-900 rounded-md">
+                        <thead>
+                           <tr className="border-b border-gray-800 text-left">
+                              <th className="p-3 uppercase text-sm font-medium text-gray-500">name</th>
+                              <th className="p-3 uppercase text-sm font-medium text-gray-500">edit</th>
+                              <th className="p-3 uppercase text-sm font-medium text-gray-500">delete</th>
+                           </tr>
+                        </thead>
+                        <tbody>
+                           {data?.categories?.map(category => (
+                              <tr key={category._id} className="odd:bg-gray-800">
+                                 <td className="p-3 capitalize text-sm font-normal text-gray-400">{category.name}</td>
+                                 <td className="p-3 capitalize text-sm font-normal text-gray-400"><button>edit</button></td>
+                                 <td className="p-3 capitalize text-sm font-normal text-gray-400"><button>delete</button></td>
+                              </tr>
+                           ))}
+                        </tbody>
+                     </table>
+                  </div>
+                  <Pagination page={parseInt(page)} count={data.count} perPage={data.perPage} path="dashboard/categories" />
+               </> : <Spinner />
+            }
        </Wrapper>
     )
 }
